7a71b5a8004a2d352a6607002461e700b6b9bca2
ggulra <greg.gulrajani@nucleusfinancial.com>
2024-05-30 20:05:54 +0100

    Another unit missing test

--- src/test/java/com/humannative/api/auth/service/JWTServiceTest.java
+++ src/test/java/com/humannative/api/auth/service/JWTServiceTest.java
@@ -1,8 +1,13 @@
 package com.humannative.api.auth.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import com.humannative.security.service.JWTService;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class JWTServiceTest {
 
@@ -13,4 +18,8 @@ public class JWTServiceTest {
 		service = new JWTService("123", 60000);
 	}
 
+	@Test
+	void verify_expirtyTime_matches() {
+		assertThat(service.getExpiryTime(), is(60000L));
+	}
 }
